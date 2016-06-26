e3afdbc76f2f337039819834c57212b1eeafd015
Stefan Scherer <scherer_stefan@icloud.com>
2016-06-26 15:54:42 +0200

    Simplify setup for 1.12.0-rc2

--- swarm/buildswarm-do.sh
+++ swarm/buildswarm-do.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-LINK=https://test.docker.com/builds/Linux/x86_64/docker-1.12.0-rc1.tgz
-
 SIZE=2gb
 REGION=ams2
 IMAGE=ubuntu-15-10-x64
@@ -15,8 +13,8 @@ docker-machine create \
   --digitalocean-region=${REGION} \
   --digitalocean-private-networking=true \
   --digitalocean-image=${IMAGE} \
+  --engine-install-url=https://test.docker.com \
   ${PREFIX}-sw01
-echo "sudo /etc/init.d/docker stop && curl $LINK | tar xzf - && sudo mv docker/* /usr/bin && rm -rf docker/ && sudo /etc/init.d/docker start" | docker-machine ssh ${PREFIX}-sw01 sh -
 docker-machine ssh ${PREFIX}-sw01 docker swarm init
 
 # create another swarm node
@@ -27,8 +25,8 @@ docker-machine create \
   --digitalocean-region=${REGION} \
   --digitalocean-private-networking=true \
   --digitalocean-image=${IMAGE} \
+  --engine-install-url=https://test.docker.com \
   ${PREFIX}-sw02
-echo "sudo /etc/init.d/docker stop && curl $LINK | tar xzf - && sudo mv docker/* /usr/bin && rm -rf docker/ && sudo /etc/init.d/docker start" | docker-machine ssh ${PREFIX}-sw02 sh -
 docker-machine ssh ${PREFIX}-sw02 docker swarm join $(docker-machine ip ${PREFIX}-sw01):2377
 
 # list nodes
