276389c5ca5b65ef1d824e9f04fc3878623dae0f
spara <sophia.parafina@gmail.com>
2016-09-25 05:07:37 -0700

    added aspnet-web example

---
+++ windows/aspnet-web/README.md
@@ -0,0 +1,3 @@
+# ASP.NET Web application
+
+A simple example using asp.net to serve a web page using kestrel. You can use either docker-compose or docker run to start the image.
---
+++ windows/aspnet-web/webserver/app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
